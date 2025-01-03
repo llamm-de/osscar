77155bacd9f630493ada4ab52fd14623b489c2df
Lukas Lamm <lamm.lukas@gmail.com>
2025-01-03 16:16:07 +0100

    Minimal setup and boilerplate for project

---
+++ README.md
@@ -0,0 +1,7 @@
+# OSScar
+
+A simple tool to track the open source license compliance of your projects dependencies throught the use of SBOMs.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='osscar',  # Updated project name
+    name='osscar',
     version='0.1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'osscar_command=osscar.main:main',  # Updated command name
+            # 'osscar_command=osscar.main:main'
         ],
     },
     install_requires=[
@@ -14,11 +14,11 @@ setup(
     author='Lukas Lamm',
     author_email='lamm.lukas@gmail.com',
     description='A brief description of your project',
-    url='https://github.com/yourusername/osscar',  # Updated URL
+    url='https://github.com/llamm-de/osscar',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.11',
 )
