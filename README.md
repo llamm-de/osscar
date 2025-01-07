8de19e32537dc4c3d442d0e9099d0a7190191735
Lukas Lamm <lamm.lukas@gmail.com>
2025-01-07 21:27:51 +0100

    Added test

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # OSScar
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![Python 3.9+](https://img.shields.io/badge/python-3.9+-blue.svg)](https://www.python.org/downloads/)
 [![Coverage](https://codecov.io/gh/llamm-de/osscar/branch/main/graph/badge.svg)](https://codecov.io/gh/llamm-de/osscar)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Workflow Status]](https://github.com/llamm-de/osscar/actions/workflows/ci.yml/badge.svg)
 
 A simple tool to track the open source license compliance of your projects dependencies through the use of SBOMs.
 
--- tests/test_license.py
+++ tests/test_license.py
@@ -14,6 +14,10 @@ def test_license_repr():
     license_instance = License(name=LicenseName.MIT, compatible_licenses=[LicenseName.MIT, LicenseName.APACHE_2_0], copyleft=True)
     assert repr(license_instance) == "License(LicenseName.MIT)"
 
+def test_is_compatible_with_self():
+    license1 = License(name=LicenseName.MIT, compatible_licenses=[LicenseName.MIT, LicenseName.APACHE_2_0], copyleft=True)
+    assert license1.is_compatible(license1)  # Should be True
+
 def test_is_compatible_with_compatible_license():
     license1 = License(name=LicenseName.MIT, compatible_licenses=[LicenseName.MIT, LicenseName.APACHE_2_0], copyleft=True)
     license2 = License(name=LicenseName.APACHE_2_0, compatible_licenses=[LicenseName.MIT, LicenseName.APACHE_2_0], copyleft=True)
